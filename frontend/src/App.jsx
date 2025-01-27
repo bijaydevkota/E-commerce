318519728592488cf8d4d848f9d2a85c9941241f
bijay <dbijay2058@gmail.com>
2025-01-27 12:45:03 +0545

    banner slider added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import TopNavbar from "./components/top-navbar";
 import MidNavbar from "./components/mid-navbar";
 import BottomNavbar from "./components/bottom-navbar";
 import HeroSectionSlider from "./components/hero-section-slider";
+import FeatureCategoriesSection from "./components/feature-categories-section";
+import BannerSection from "./components/banner-section";
 
 export default function App() {
   return (
@@ -14,6 +16,8 @@ export default function App() {
       <BottomNavbar/>
       <hr className="text-gray-300 mt-4"/>
       <HeroSectionSlider/>
+      <FeatureCategoriesSection/>
+      <BannerSection/>
     </div>
   );
 }
--- frontend/src/components/hero-section-slider.jsx
+++ frontend/src/components/hero-section-slider.jsx
@@ -15,12 +15,12 @@ import slider2 from "../../public/slider2.jpg";
 export default function HeroSectionSlider() {
   return (
     <div className="w-8/12 mx-auto mt-6">
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper rounded-md">
         <SwiperSlide>
-            <img src={slider1} alt="slider1" />
+            <img src={slider1} alt="slider1" className="h-[65vh] object-cover"/>
         </SwiperSlide>
         <SwiperSlide>
-            <img src={slider2} alt="slider2" />
+            <img src={slider2} alt="slider2" className="h-[65vh] object-cover"/>
         </SwiperSlide>
       </Swiper>
     </div>
